6cdd78be433b23374458b64d0d4a5dcd0510d6a1
emuuti <tilda.backhans@gmail.com>
2025-09-08 14:38:11 +0200

    trial

---
+++ src/main/java/org/example/Trial.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Trial {
+    public static int seven() {
+        return 7;
+    }
+}
---
+++ src/test/java/org/example/TrialTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrialTest {
+
+    @Test
+    public void addTest() {
+        int s = Trial.seven();
+        assertEquals(7, s);
+    }
+}
