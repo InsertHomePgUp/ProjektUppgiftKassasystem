6821009f8dd031e7badec89d7d96a88020d5ee2a
emuuti <tilda.backhans@gmail.com>
2025-10-13 23:09:59 +0200

    customer klass

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,21 +1,53 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Customer {
 
-    private final long personalIdentityNumber;
-    private final long phoneNumber;
+    private final String personalIdentityNumber;
+    private final String phoneNumber;
     private final String email;
     private final String name;
+    private boolean hasMembership;
+    private final ArrayList<Item> boughtItemList;
 
-    public Customer(String name, long personalIdentityNumber, long phoneNumber, String email) {
-        this.name = name;
-        this.personalIdentityNumber = personalIdentityNumber;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+    public Customer(String name, String personalIdentityNumber, String phoneNumber, String email) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (personalIdentityNumber.trim().isEmpty()) {
+            throw new IllegalArgumentException("Personal identity number cannot be null or empty");
+        }
+        if (phoneNumber.trim().isEmpty()) {
+            throw new IllegalArgumentException("Phone number cannot be null or empty");
+        }
+        if (email.trim().isEmpty()) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+        this.name = Objects.requireNonNull(name);
+        this.personalIdentityNumber = Objects.requireNonNull(personalIdentityNumber);
+        this.phoneNumber = Objects.requireNonNull(phoneNumber);
+        this.email = Objects.requireNonNull(email);
+        this.boughtItemList = new ArrayList<Item>();
+        this.hasMembership = false;
     }
-    public long getPersonalIdentityNumber() {return personalIdentityNumber;}
-    public long getPhoneNumber() {return phoneNumber;}
+    public String getPersonalIdentityNumber() {return personalIdentityNumber;}
+    public String getPhoneNumber() {return phoneNumber;}
     public String getEmail() {return email;}
     public String getName() {return name;}
-
+    public boolean getMembershipStatus() {return hasMembership;}
+    public void setMembershipStatus(boolean areTheyAMember) {hasMembership = areTheyAMember;}
+    public ArrayList<Item> getBoughtItemList() {
+        return new ArrayList<>(boughtItemList);
+    }
+    public boolean addItem(Item item) {
+        boughtItemList.add(item);
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "Name: " + name + ", PID: " + personalIdentityNumber +
+                ", Phone Number: " + phoneNumber + ", Email: " + email;
+    }
 }
--- src/test/java/org/example/CustomerTest.java
+++ src/test/java/org/example/CustomerTest.java
@@ -1,15 +1,97 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomerTest {
-    void ConstructorTest() {
-        Customer customer = new Customer("Namn Namnson",
-                8907028631, 0700143541, "Namn@Namnson.com");
-        assertEquals("Namn Namnson", customer.getName());
-        assertEquals(8907028631, customer.getPersonalIdentityNumber());
-        assertEquals(0700143541, customer.getPhoneNumber());
-        assertEquals("Namn@Namnson.com", customer.getEmail());
+
+    private Customer customer;
+
+    @BeforeEach
+    public void setUp() {
+        customer = new Customer("Jane Doe", "8907028631", "0700143541", "Jane@Doe.com");
     }
+
+    @Nested
+    @DisplayName("Constructor")
+    class ConstructorTests {
+        @Test
+        void ConstructorTest() {
+            assertEquals("Jane Doe", customer.getName());
+            assertEquals("8907028631", customer.getPersonalIdentityNumber());
+            assertEquals("0700143541", customer.getPhoneNumber());
+            assertEquals("Jane@Doe.com", customer.getEmail());
+            assertFalse(customer.getMembershipStatus());
+        }
+        //testa om tomma parametrar ger felmeddelande
+        @Test
+        void EmptyNameParameterTest() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> new Customer(" ", "8907028631", "0700143541", "Jane@Doe.com"));
+        }
+        @Test
+        void EmptyPIDParameterTest() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> new Customer("Jane Doe", " ", "0700143541", "Jane@Doe.com"));
+        }
+        @Test
+        void EmptyPhoneNumberParameterTest() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> new Customer("Jane Doe", "8907028631", " ", "Jane@Doe.com"));
+        }
+        @Test
+        void EmptyEmailParameterTest() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> new Customer("Jane Doe", "8907028631", "0700143541", " "));
+        }
+
+        //null i parametrar
+        @Test
+        void NullNameParameterTest() {} {
+            assertThrows(NullPointerException.class,
+                    () -> new Customer(null, "8907028631", "0700143541", "Jane@Doe.com"));
+        }
+        @Test
+        void NullPIDParameterTest() {} {
+            assertThrows(NullPointerException.class,
+                    () -> new Customer("Jane Doe", null, "0700143541", "Jane@Doe.com"));
+        }
+        @Test
+        void NullPhoneNumberParameterTest() {} {
+            assertThrows(NullPointerException.class,
+                    () -> new Customer("Jane Doe", "8907028631", null, "Jane@Doe.com"));
+        }
+        @Test
+        void NullEmailParameterTest() {} {
+            assertThrows(NullPointerException.class,
+                    () -> new Customer("Jane Doe", "8907028631", "0700143541", null));
+        }
+    }
+
+    @Nested
+    @DisplayName("Item List Tests")
+    class ItemListTests {
+        //testa om man kan ändra i den returnerade listan
+        //testa att lägga till en sak i listan
+        //testa att det inte finns dubs
+    }
+
+
+    //testa toString
+
+
+    @Test
+    void toStringTest() {
+        assertEquals("Name: Jane Doe, PID: 8907028631, Phone Number: 0700143541, Email: Jane@Doe.com", customer.toString());
+    }
+
+    @Test
+    void setToHasMembership() {
+        customer.setMembershipStatus(true);
+        assertTrue(customer.getMembershipStatus());
+    }
+
 }
