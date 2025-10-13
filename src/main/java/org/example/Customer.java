ea22487d2fab3ae61b76ccdcd9aa6381b7ff4296
emuuti <tilda.backhans@gmail.com>
2025-10-13 21:42:20 +0200

    customer klass

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class Customer {
+
+    private final long personalIdentityNumber;
+    private final long phoneNumber;
+    private final String email;
+    private final String name;
+
+    public Customer(String name, long personalIdentityNumber, long phoneNumber, String email) {
+        this.name = name;
+        this.personalIdentityNumber = personalIdentityNumber;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    public long getPersonalIdentityNumber() {return personalIdentityNumber;}
+    public long getPhoneNumber() {return phoneNumber;}
+    public String getEmail() {return email;}
+    public String getName() {return name;}
+
 }
--- src/test/java/org/example/CustomerTest.java
+++ src/test/java/org/example/CustomerTest.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomerTest {
+    void ConstructorTest() {
+        Customer customer = new Customer("Namn Namnson",
+                8907028631, 0700143541, "Namn@Namnson.com");
+        assertEquals("Namn Namnson", customer.getName());
+        assertEquals(8907028631, customer.getPersonalIdentityNumber());
+        assertEquals(0700143541, customer.getPhoneNumber());
+        assertEquals("Namn@Namnson.com", customer.getEmail());
+    }
 }
