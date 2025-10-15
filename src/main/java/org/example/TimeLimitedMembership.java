98f8f0b40acaba2df03af9dd41001b47bf11eee9
emuuti <tilda.backhans@gmail.com>
2025-10-15 13:29:28 +0200

    customer klass

--- src/main/java/org/example/TimeLimitedMembership.java
+++ src/main/java/org/example/TimeLimitedMembership.java
@@ -11,14 +11,7 @@ public class TimeLimitedMembership implements Membership {
 
     @Override
     public HashMap<Item, Double> getDiscounts(){
-        ItemType vegetable = new ItemType("vegetable", 1.0, 0, 0);
-        Money cost = new Money(SEK.instance, 500);
-        Item carrot = new Item("carrot", vegetable, cost);
-
-        HashMap<Item, Double> discounts = new HashMap<>();
-
-        discounts.putIfAbsent(carrot, 0.2);
-        return discounts;
+        return null;
     };
 
     @Override
